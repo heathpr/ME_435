26cd15f084a89c62feee74fb309ef0fa82b20e6c
peter heath <heathpr@rose-hulman.edu>
2016-03-17 12:27:45 -0400

    fixed a variable name

--- MATLAB-DH-Helper-Functions/homogeneousTransformation.m
+++ MATLAB-DH-Helper-Functions/homogeneousTransformation.m
@@ -1,26 +1,26 @@
-function T = homogeneousTransformation(a, d, degreesAlpha, degreesTheta)
+function A = homogeneousTransformation(a, d, degreesAlpha, degreesTheta)
 % Creates a DH homogeneous transformation matrix from DH parameters.
 alpha = degreesAlpha/180*pi;  % Convert to radians
 theta = degreesTheta/180*pi;  % Convert to radians
-T = zeros(4,4);
+A = zeros(4,4);
 
-T(1,1) = cos(theta);
-T(1,2) = -sin(theta)*cos(alpha);
-T(1,3) = sin(theta)*sin(alpha);
-T(1,4) = a*cos(theta);
+A(1,1) = cos(theta);
+A(1,2) = -sin(theta)*cos(alpha);
+A(1,3) = sin(theta)*sin(alpha);
+A(1,4) = a*cos(theta);
 
-T(2,1) = sin(theta);
-T(2,2) = cos(theta)*cos(alpha);
-T(2,3) = -cos(theta)*sin(alpha);
-T(2,4) = a*sin(theta);
+A(2,1) = sin(theta);
+A(2,2) = cos(theta)*cos(alpha);
+A(2,3) = -cos(theta)*sin(alpha);
+A(2,4) = a*sin(theta);
 
-T(3,1) = 0;
-T(3,2) = sin(alpha);
-T(3,3) = cos(alpha);
-T(3,4) = d;
+A(3,1) = 0;
+A(3,2) = sin(alpha);
+A(3,3) = cos(alpha);
+A(3,4) = d;
 
-T(4,1) = 0;
-T(4,2) = 0;
-T(4,3) = 0;
-T(4,4) = 1;
+A(4,1) = 0;
+A(4,2) = 0;
+A(4,3) = 0;
+A(4,4) = 1;
 end
